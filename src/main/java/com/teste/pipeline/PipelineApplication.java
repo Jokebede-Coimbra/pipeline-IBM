cc72ff6e4ffaafc2668173fa01f01779143897e8
Coimbra <adsjkbd@gmail.com>
2022-08-31 18:09:16 -0300

    Adicionando a classe CPF

--- src/main/java/com/teste/pipeline/PipelineApplication.java
+++ src/main/java/com/teste/pipeline/PipelineApplication.java
@@ -1,13 +1,24 @@
 package com.teste.pipeline;
 
-import org.springframework.boot.SpringApplication;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PipelineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PipelineApplication.class, args);
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in));
+
+		System.out.println("informe o cpf: ");
+		String cpf = reader.readLine();
+
+		System.out.println("O cpf digitado é: " +
+				(ValidadorCpf.validarCpf(cpf) ? "Verdadeiro" : "falso"));
+
 	}
 
 }
--- src/test/java/com/teste/pipeline/PipelineApplicationTests.java
+++ src/test/java/com/teste/pipeline/PipelineApplicationTests.java
@@ -1,5 +1,7 @@
 package com.teste.pipeline;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PipelineApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void cpfValido() {
+		var valido = ValidadorCpf.validarCpf("615.284.680-31");
+		assertEquals(true, valido);
+	}
+
+	@Test
+	void cpfInvalido() {
+		var invalido = ValidadorCpf.validarCpf("88888888888");
+		assertEquals(true, invalido);
 	}
 
 }
